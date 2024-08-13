e15893c62e33150bc1cd3170c54afb5fb7709e0c
Beamlak996 <beamlaktsegaye996@gmail.com>
2024-08-13 03:28:02 -0900

    modariation done

--- server/moderation/index.js
+++ server/moderation/index.js
@@ -1,11 +1,31 @@
-import express from "express"
-import bodyParser from "body-parser"
-import axios from "axios"
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.listen(4003, ()=> {
-    console.log("Listening to port 4003")
-})
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    await axios.post("http://localhost:4005/events", {
+        type: "CommentModerated",
+        data: {
+            id: data.id,
+            postId: data.postId,
+            content: data.content,
+            status: status
+        }
+    });
+  }
+
+  res.send({})
+});
+
+app.listen(4003, () => {
+  console.log("Listening to port 4003");
+});
